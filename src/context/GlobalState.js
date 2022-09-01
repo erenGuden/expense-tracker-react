eae1db9ecd111ccd5fce362cc216462dcb3e23e1
erenGuden <105981672+erenGuden@users.noreply.github.com>
2022-09-01 14:28:25 -0500

    fix globalState bug

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -13,7 +13,7 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState, () => {
     const localData = localStorage.getItem('state');
-    return localData ? JSON.parse(localData) : [];
+    return localData ? JSON.parse(localData) : initialState;
   });
   useEffect(() => {
     localStorage.setItem('state', JSON.stringify(state))
